561183defbd2b316948ae1ce73eaa5becd711e18
Christian Ledermann <christian.ledermann@gmail.com>
2013-07-22 15:22:45 +0300

    add extract timestamp and altitude from exif data

--- collective/geo/exif/readexif.py
+++ collective/geo/exif/readexif.py
@@ -1,6 +1,6 @@
 # https://gist.github.com/moshekaplan/5330395
 import sys
-
+import pytz, datetime
 from PIL import Image
 from PIL.ExifTags import TAGS, GPSTAGS
 
@@ -17,11 +17,9 @@ def get_exif_data(image):
                 for gps_tag in value:
                     sub_decoded = GPSTAGS.get(gps_tag, gps_tag)
                     gps_data[sub_decoded] = value[gps_tag]
-
                 exif_data[decoded] = gps_data
             else:
                 exif_data[decoded] = value
-
     return exif_data
 
 def _convert_to_degress(value):
@@ -38,28 +36,64 @@ def _convert_to_degress(value):
     return d + (m / 60.0) + (s / 3600.0)
 
 def get_lat_lon(exif_data):
-    """Returns the latitude and longitude, if available, from the provided exif_data (obtained through get_exif_data above)"""
+    """Returns the latitude and longitude, if available, from the
+    provided exif_data (obtained through get_exif_data above)"""
     lat = None
     lon = None
-
     if "GPSInfo" in exif_data:
         gps_info = exif_data["GPSInfo"]
-
         gps_latitude = gps_info.get("GPSLatitude")
         gps_latitude_ref = gps_info.get('GPSLatitudeRef')
         gps_longitude = gps_info.get('GPSLongitude')
         gps_longitude_ref = gps_info.get('GPSLongitudeRef')
-
         if gps_latitude and gps_latitude_ref and gps_longitude and gps_longitude_ref:
             lat = _convert_to_degress(gps_latitude)
             if gps_latitude_ref != "N":
                 lat *= -1
-
             lon = _convert_to_degress(gps_longitude)
             if gps_longitude_ref != "E":
                 lon *= -1
-
     return lat, lon
+
+
+def get_altitude(exif_data):
+    """ extract altitude if avalaible from the
+    provided exif_data (obtained through get_exif_data above)"""
+    alt = None
+    if "GPSInfo" in exif_data:
+        gps_info = exif_data["GPSInfo"]
+        gps_altitude = gps_info.get('GPSAltitude')
+        gps_altitude_ref = gps_info.get('GPSAltitudeRef')
+        if gps_altitude:
+            alt = float(gps_altitude[0]) / float(gps_altitude[1])
+            if gps_altitude_ref == 1:
+                alt *=-1
+    return alt
+
+def get_timestamp(exif_data):
+    """ extract the timestamp if avalaible as datetime  from the
+    provided exif_data (obtained through get_exif_data above)"""
+    dt = None
+    utc = pytz.utc
+    if "GPSInfo" in exif_data:
+        gps_info = exif_data["GPSInfo"]
+        gps_time_stamp = gps_info.get('GPSTimeStamp')
+        if 'GPSDateStamp' in gps_info:
+            gps_date = [int(i) for i in gps_info['GPSDateStamp'].split(':')]
+        elif 29 in gps_info:
+            gps_date = [int(i) for i in gps_info[29].split(':')]
+        else:
+            gps_date = None
+        if gps_time_stamp and gps_date:
+            yy = gps_date[0]
+            mm = gps_date[1]
+            dd = gps_date[2]
+            h = int(float(gps_time_stamp[0][0]) / float(gps_time_stamp[0][1]))
+            m = int(float(gps_time_stamp[1][0]) / float(gps_time_stamp[1][1]))
+            s = int(float(gps_time_stamp[2][0]) / float(gps_time_stamp[2][1]))
+            dt = utc.localize(datetime.datetime(yy,mm,dd,h,m,s))
+    return dt
+
 
 
 ################
--- collective/geo/exif/utils.py
+++ collective/geo/exif/utils.py
@@ -4,7 +4,7 @@ from PIL import Image
 
 from collective.geo.contentlocations.interfaces import IGeoManager
 from collective.geo.settings.interfaces import IGeoCustomFeatureStyle, IGeoFeatureStyle
-from collective.geo.exif.readexif import get_exif_data, get_lat_lon
+from collective.geo.exif.readexif import get_exif_data, get_lat_lon, get_timestamp
 
 
 def set_geoannotation(context, icon):
@@ -14,20 +14,24 @@ def set_geoannotation(context, icon):
             image = Image.open(context.getFile().getIterator())
             exif_data = get_exif_data(image)
             lat, lon = get_lat_lon(exif_data)
+            timestamp = get_timestamp(exif_data)
         except:
             return None
         if lat and lon and geo.isGeoreferenceable():
             geo.setCoordinates('Point', (lon, lat))
-            style.geostyles.data['use_custom_styles']=True
             if icon:
+                style.geostyles.data['use_custom_styles']=True
                 style.geostyles.data['marker_image'] = icon
                 style.geostyles.data['marker_image_size'] = 1.0
-            style.geostyles.update(style.geostyles)
+                style.geostyles.update(style.geostyles)
             logger.info('annotated %s with lat %f, lon %f' % (context.getId(), lat, lon))
             success = True
         else:
             success = False
             logger.info('Image has no EXIF GPS information')
+            timestamp = exif_data.get('DateTimeOriginal')
+        if timestamp:
+            context.setCreationDate(timestamp)
         return success
 
 def handle_add_image(context, event):
